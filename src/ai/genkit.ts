850513bc336da95a51145fd51b46b45a60e53cb5
account <account@lithiumtech.co>
2025-12-05 01:15:15 +0000

    modified:   src/ai/genkit.ts

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,11 +1,9 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
-const GEMINI_KEY = "AIzaSyBo028lDGC2e6p7tqrBr8CWYXu66qDAujI";
-
 export const ai = genkit({
   plugins: [
-    googleAI({ apiKey: GEMINI_KEY })
+    googleAI({ apiKey: process.env.GEMINI_API_KEY })
   ],
   model: 'googleai/gemini-1.5-flash',
 });
